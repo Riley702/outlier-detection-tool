09caf8c65a7a14b17e7a1ff07a528cf8320fcf39
Yisong Chen <35907236+Riley702@users.noreply.github.com>
2025-02-22 18:43:18 -0500

    Update data_analysis.py

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import logging
 
-# Configure logg
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 def calculate_statistics(data, column):
